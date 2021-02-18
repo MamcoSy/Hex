c5055cfd372795d535a38fdf89d7c1575a7ed643
MamcoSy <mamco.aly.sy@gmail.com>
2021-02-18 21:18:27 +0000

    documenting DatabaseConnectionException

--- src/Hex/DatabaseConnection/Exception/DatabaseConnectionException.php
+++ src/Hex/DatabaseConnection/Exception/DatabaseConnectionException.php
@@ -8,6 +8,12 @@ use PDOException;
 
 class DatabaseConnectionException extends PDOException
 {
+    /**
+     * Override constructor to avoid PHP BUGS #51742, #39615
+     *
+     * @param string  $message
+     * @param integer $code
+     */
     public function __construct(string $message, int $code)
     {
         $this->message = $message;
